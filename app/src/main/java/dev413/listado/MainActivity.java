02b5503177697c584e330b93a3a2fb06efd02e25
Iván <diskman199@gmail.com>
2018-05-17 11:34:48 -0500

    Test 3

--- app/src/main/java/dev413/listado/MainActivity.java
+++ app/src/main/java/dev413/listado/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //:
         setContentView(R.layout.activity_main);
         ListView laLista = (ListView) findViewById(R.id.theList);
         OrigenDeDatosDeTarjeta data = new OrigenDeDatosDeTarjeta();
